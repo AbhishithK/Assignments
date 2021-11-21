d76498aebe857ae2a2132b0e1e9e09928d59066b
AbhishithK <94011399+AbhishithK@users.noreply.github.com>
2021-11-21 07:25:47 +0530

    Add files via upload

---
+++ calculate the number of upper case letters and lower case letters solution.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def upper_lower_count(l):
+    global upper
+    global lower
+    upper = 0
+    lower = 0
+    for value in l:
+        if value.isupper():
+            upper+=1
+        elif value.islower():
+            lower+=1
+    return upper
+    return lower
+
+s = 'The quick Brow Fox'
+result = upper_lower_count(s)
+print('No. of Upper case characters : ',upper)
+print('No. of Upper case characters : ',lower)
+
+
+# In[ ]:
+
+
+
+
