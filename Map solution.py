ab6465a3bc9ce2397e68251fd8ee38e06e3a8883
AbhishithK <94011399+AbhishithK@users.noreply.github.com>
2021-12-05 08:41:59 +0530

    Add files via upload

---
+++ Map solution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def triple(num):
+    return num*3
+a = [1, 2, 3, 4, 5, 6, 7]
+result = list(map(triple,a))
+print(result)
+
+
+# In[ ]:
+
+
+
+
