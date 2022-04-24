24aaeb3a68ed3a145b827c10ac21367bcb0c323a
AbhishithK <94011399+AbhishithK@users.noreply.github.com>
2022-04-24 22:57:47 +0530

    Add files via upload

---
+++ Make Your Own mini-Dictionary.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+my_dict = {}
+for i in range(97,97+26):
+    my_dict[chr(i)] = i
+print(my_dict)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
