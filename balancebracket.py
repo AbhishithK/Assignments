7ae049975a19b3921cd8b6d1c16c723706ddb1a3
AbhishithK <94011399+AbhishithK@users.noreply.github.com>
2022-06-21 05:58:37 +0530

    Add files via upload

---
+++ balancebracket.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def balancebracket(s):
+    stack = []
+    for i in range(len(s)):
+        if s[i]=="[" or s[i]=="(" or s[i]=="{":
+            stack.append(s[i])
+        elif len(stack)!=0 and stack[-1] =="[" and s[i]=="]":
+            stack.pop()
+        elif len(stack)!=0 and stack[-1] =="{" and s[i]=="}":
+            stack.pop()
+        elif len(stack)!=0 and stack[-1] =="(" and s[i]==")":
+            stack.pop()
+        else:
+            return False
+
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+    
+s = "([])()()"
+function = balancebracket(s)
+print(function)
+            
+
